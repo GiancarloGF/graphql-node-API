78188c3d4c96038e0d22950f285cfcc6745a1f4f
Giancarlo Guerra Farfan <gian.guerra23@gmail.com>
2022-02-08 21:12:08 -0500

    Post and Comment Type created

--- graphql/dataTypes.js
+++ graphql/dataTypes.js
@@ -1,17 +1,68 @@
-const { GraphQLString, GraphQLID, GraphQLObjectType } = require('graphql');
-
-const UserType = new GraphQLObjectType({
-	name: 'UserType',
-	description: 'The user data type',
-	fields: {
-		id: { type: GraphQLID },
-		username: { type: GraphQLString },
-		email: { type: GraphQLString },
-		// password: { type: GraphQLString },
-        displayname:{ type: GraphQLString},
-        createdAt: { type: GraphQLString},
-        updatedAt: { type: GraphQLString}
-	},
-});
-
-module.exports = { UserType };
+const {
+    GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLList,
+  } = require("graphql");
+  const { Post, Comment, User } = require("../models");
+  
+  const UserType = new GraphQLObjectType({
+    name: "User",
+    description: "User type",
+    fields: () => ({
+      id: { type: GraphQLID },
+      username: { type: GraphQLString },
+      email: { type: GraphQLString },
+    // password: { type: GraphQLString },
+      displayName: { type: GraphQLString },
+    }),
+  });
+  
+  const PostType = new GraphQLObjectType({
+    name: "Post",
+    description: "Post Type",
+    fields: () => ({
+      id: { type: GraphQLID },
+      title: { type: GraphQLString },
+      body: { type: GraphQLString },
+      author: {
+        type: UserType,
+        resolve(parent) {
+          return User.findById(parent.authorId);
+        },
+      },
+      comments: {
+        type: new GraphQLList(CommentType),
+        resolve(parent) {
+          return Comment.find({ postId: parent.id });
+        },
+      },
+    }),
+  });
+  
+  const CommentType = new GraphQLObjectType({
+    name: "Comment",
+    description: "comments type",
+    fields: () => ({
+      id: { type: GraphQLID },
+      comment: { type: GraphQLString },
+      user: {
+        type: UserType,
+        resolve(parent) {
+          return User.findById(parent.userId);
+        },
+      },
+      post: {
+        type: PostType,
+        resolve(parent) {
+          return Post.findById(parent.postId);
+        },
+      },
+    }),
+  });
+  
+  module.exports = {
+    UserType,
+    PostType,
+    CommentType,
+  };
