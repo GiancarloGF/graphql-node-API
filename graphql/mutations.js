b0a10f43470b98cfcdf29809b0ce9416dabf025d
Giancarlo Guerra Farfan <gian.guerra23@gmail.com>
2022-02-08 21:38:06 -0500

    feat: more Post mutations created

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -68,5 +68,123 @@ const createPost = {
 		return post.save();
 	},
 };
+const updatePost = {
+	type: PostType,
+	description: 'update a blog post',
+	args: {
+		id: { type: new GraphQLNonNull(GraphQLID) },
+		title: { type: new GraphQLNonNull(GraphQLString) },
+		body: { type: new GraphQLNonNull(GraphQLString) },
+	},
+	async resolve(_, { id, title, body }, { verifiedUser }) {
+		if (!verifiedUser) throw new Error('Unauthorized');
 
-module.exports = { register, login, createPost };
+		const postUpdated = await Post.findOneAndUpdate(
+			{ _id: id, authorId: verifiedUser._id },
+			{ title, body },
+			{
+				new: true,
+				runValidators: true,
+			}
+		);
+
+		if (!postUpdated) throw new Error('No post for given id');
+
+		return postUpdated;
+	},
+};
+
+const deletePost = {
+	type: GraphQLString,
+	description: 'Delete post',
+	args: {
+		postId: { type: new GraphQLNonNull(GraphQLID) },
+	},
+	async resolve(_, args, { verifiedUser }) {
+		const postDeleted = await Post.findOneAndDelete({
+			_id: args.postId,
+			authorId: verifiedUser._id,
+		});
+		if (!postDeleted) throw new Error('No post with given ID Found for the author');
+
+		return 'Post deleted';
+	},
+};
+
+const addComment = {
+	type: CommentType,
+	description: 'Create a new comment for a blog post',
+	args: {
+		comment: { type: new GraphQLNonNull(GraphQLString) },
+		postId: { type: new GraphQLNonNull(GraphQLID) },
+	},
+	resolve(_, { postId, comment }, { verifiedUser }) {
+		const newComment = new Comment({
+			userId: verifiedUser._id,
+			postId,
+			comment,
+		});
+		return newComment.save();
+	},
+};
+
+const updateComment = {
+	type: CommentType,
+	description: 'update a comment',
+	args: {
+		id: { type: new GraphQLNonNull(GraphQLID) },
+		comment: { type: new GraphQLNonNull(GraphQLString) },
+	},
+	async resolve(_, { id, comment }, { verifiedUser }) {
+		if (!verifiedUser) throw new Error('UnAuthorized');
+
+		const commentUpdated = await Comment.findOneAndUpdate(
+			{
+				_id: id,
+				userId: verifiedUser._id,
+			},
+			{
+				comment,
+			},
+			{
+				new: true,
+				runValidators: true,
+			}
+		);
+
+		if (!commentUpdated) throw new Error('No comment with the given ID');
+
+		return commentUpdated;
+	},
+};
+
+const deleteComment = {
+	type: GraphQLString,
+	description: 'delete a comment',
+	args: {
+		id: { type: new GraphQLNonNull(GraphQLID) },
+	},
+	async resolve(_, { id }, { verifiedUser }) {
+		if (!verifiedUser) throw new Error('Unauthorized');
+
+		const commentDelete = await Comment.findOneAndDelete({
+			_id: id,
+			userId: verifiedUser._id,
+		});
+
+		if (!commentDelete) throw new Error('No comment with the given ID for the user');
+
+		return 'Comment deleted';
+	},
+};
+
+module.exports = {
+	register,
+	login,
+	createPost,
+	addComment,
+	updatePost,
+	deletePost,
+	updateComment,
+	deleteComment,
+};
